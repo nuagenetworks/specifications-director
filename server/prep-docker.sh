9efa4149843d441c6e7e3c22cc77c8594922e812
Antoine Mercadal <antoine.mercadal@gmail.com>
2015-11-19 15:14:05 -0800

    use garuda as base docker image

--- server/prep-docker.sh
+++ server/prep-docker.sh
@@ -4,21 +4,6 @@ CURRENT_PATH=`pwd`
 rm -rf libs
 mkdir libs
 
-cd ../../bambou/
-python setup.py sdist
-cd $CURRENT_PATH
-cp ../../bambou/dist/bambou*.tar.gz libs
-
-cd ../../garuda/
-python setup.py sdist
-cd $CURRENT_PATH
-cp ../../garuda/dist/garuda*.tar.gz libs
-
-cd ../../monolithe/
-python setup.py sdist
-cd $CURRENT_PATH
-cp ../../monolithe/dist/monolithe*.tar.gz libs
-
 cd ..
 ../monolithe/commands/monogen-sdk -f specs -c specs/monoconf/config.ini
 cd codegen
---
+++ server/setup.py
@@ -0,0 +1,20 @@
+# -*- coding:utf-8 -*-
+
+import os
+from setuptools  import setup
+import pip
+
+setup(
+    name='specifications-director-server',
+    version='0.0.1',
+    author='Antoine Mercadal',
+    packages=[  'specsdirector',
+                'specsdirector.lib',
+                'specsdirector.plugins'],
+
+    author_email='antoine@nuagenetworks.net',
+    description='Specification Director Server Package',
+    install_requires=[str(ir.req) for ir in pip.req.parse_requirements('requirements.txt', session=pip.download.PipSession())],
+    license='TODO',
+    url='TODO'
+)
